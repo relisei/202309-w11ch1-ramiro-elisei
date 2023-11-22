9e74989ff3af3c34ccc9931bf6bd4b3a234c6d13
ramiro elisei <ramiroelisei@gmail.com>
2023-11-22 20:42:54 +0100

    Add debug to index database

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,14 +1,17 @@
+import createDebug from "debug";
 import chalk from "chalk";
 import mongoose from "mongoose";
+
+const debug = createDebug("mechas:database");
 
 const mongodbUrl = process.env.MONGODB_URL;
 
 export const connectToDataBase = async (mongodbUrl: string) => {
   try {
     await mongoose.connect(mongodbUrl);
-    console.log(chalk.green("Connected to database"));
+    debug(chalk.green("Connected to database"));
   } catch {
-    console.log(chalk.red({ error: "Couldn't connect to database" }));
+    debug(chalk.red({ error: "Couldn't connect to database" }));
     process.exit(1);
   }
 };
