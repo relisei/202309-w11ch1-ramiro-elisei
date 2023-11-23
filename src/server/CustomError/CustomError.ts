ae3b29d0d30033d5e52c32299bba091eef85b531
ramiro elisei <ramiroelisei@gmail.com>
2023-11-23 09:40:54 +0100

    Add Fix privateMessage in CustomError

--- src/server/CustomError/CustomError.ts
+++ src/server/CustomError/CustomError.ts
@@ -2,7 +2,7 @@ class CustomError extends Error {
   constructor(
     message: string,
     public statusCode: number,
-    public customError?: string,
+    public privateMessage?: string,
   ) {
     super(message);
   }
